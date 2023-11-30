02c6e10430c948cc6e5d8349de6a699258b58916
Nakamura <kisy23013@kibireha.ac.jp>
2023-11-30 16:19:32 +0900

    StringEditor.javaのJavadocを修正/コード中のコメントを追加

--- EX11_MeiboSearch/StringEditor.java
+++ EX11_MeiboSearch/StringEditor.java
@@ -6,14 +6,14 @@ import java.util.ArrayList;
  */
 public class StringEditor {
 	/**
-	 * 文字列と複数のインデックスの数値を受け取り、文字列をカンマで分割した後指定されたインデックスの場所の文字列をArrayListに格納して返します。
+	 * 文字列をカンマで分割した後指定されたインデックスの場所の文字列をArrayListに格納して返すメソッドです。
 	 * 
 	 * @return 指定されたインデックスの個数分の文字列が格納されたArrayList
 	 */
 	public ArrayList<String> splitByComma(String str, int... index) {
-		String[] splitStr = str.split(",");
+		String[] splitStr = str.split(",");	// String.splitメソッド:文字列を、指定した文字で分割して配列に格納するメソッド。
 		ArrayList<String> arraySplitData = new ArrayList<>();
-		for(int i = 0; i < index.length; i++) {
+		for(int i = 0; i < index.length; i++) {	// 引数indexで指定された場所の文字列をArrayListに格納していく
 			arraySplitData.add(splitStr[index[i]]);
 		}
 		
